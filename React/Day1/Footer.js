528b7f93f815b664e8115405997c9649f9b2e558
Vipul Krishna Saxena <112557719+vipulsaxena31@users.noreply.github.com>
2024-04-18 00:41:46 +0530

    Create Footer.js

---
+++ React/Day1/Footer.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer>
+        <p> &copy; 2024 My app. All right  reserved</p>
+        <nav>
+            <ul>
+                <li>Terms of services</li>
+                <li>Privacy Policy</li>
+            </ul>
+        </nav>
+    </footer>
+  );
+};
+
+export default Footer
