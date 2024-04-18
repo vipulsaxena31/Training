e54bc2818b8fe8be755103d4d328a3175f5ba4c0
Vipul Krishna Saxena <112557719+vipulsaxena31@users.noreply.github.com>
2024-04-18 23:15:52 +0530

    Create Counter.js
    Counter component i.e increment or decrement

---
+++ Testing/Day2/Counter.js
@@ -0,0 +1,23 @@
+import React, { useState } from 'react';
+
+const Counter = ({ initialCount }) => {
+  const [count, setCount] = useState(initialCount);
+
+  const increment = () => {
+    setCount(count + 1);
+  };
+
+  const decrement = () => {
+    setCount(count - 1);
+  };
+
+  return (
+    <div>
+      <p>Count: {count}</p>
+      <button onClick={increment}>Increment</button>
+      <button onClick={decrement}>Decrement</button>
+    </div>
+  );
+};
+
+export default Counter;
